4a86926b42d412b8a9cf0131edfb1b282addf95a
sumitchaudhary9877 <sumitchaudhary9877@gmail.com>
2021-07-13 01:10:38 +0530

    updated  maps token

--- src/views/Maps.js
+++ src/views/Maps.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react'
 import mapboxgl from 'mapbox-gl';
-mapboxgl.accessToken = 'pk.eyJ1Ijoib3BhbHRyb2xsIiwiYSI6ImNrcjEwYm15NjF4OHQycXFhNGJrY2F2ajAifQ.d4yBYQ1HYT7SEhz-C5lGgg';
+mapboxgl.accessToken = 'pk.eyJ1IjoiaW5mZW5zIiwiYSI6ImNrcHJpd2t1czA4dHAyb21ucmEyN2hlNzAifQ.UqPpun5dr8HdvlPPrRvx6A';
 export default function Maps() {
     const mapContainer = useRef(null);
     const map = useRef(null);
@@ -11,7 +11,7 @@ export default function Maps() {
         if (map.current) return; // initialize map only once
         map.current = new mapboxgl.Map({
             container: mapContainer.current,
-            style: 'mapbox://styles/opaltroll/ckr10d64b09uf17qo9nja1lt4',
+            style: 'mapbox://styles/infens/ckprj2dzt00zi19no4zckc7hv',
             // center: [lng, lat],
             // zoom: zoom
         });
